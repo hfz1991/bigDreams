cc517ad51e84e0c726da4f1df99ec89d4dd981f3
Charles <funzho@gmail.com>
2015-08-15 17:46:24 +1000

    init ios project

---
+++ ios/BigDream/BigDream/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BigDream
+//
+//  Created by Fangzhou He on 15/08/2015.
+//  Copyright (c) 2015 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios/BigDream/BigDream/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BigDream
+//
+//  Created by Fangzhou He on 15/08/2015.
+//  Copyright (c) 2015 Charles. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
