add4f75f1a93bd1ffd1a07ee0a070fc1df0ce28f
Charles <funzho@gmail.com>
2015-08-15 21:06:55 +1000

    finish authentication

--- ios/ParseStarterProject/ParseStarterProject/LoginViewController.m
+++ ios/ParseStarterProject/ParseStarterProject/LoginViewController.m
@@ -8,6 +8,7 @@
 
 #import "LoginViewController.h"
 #import <Parse/Parse.h>
+#import <JGProgressHUD/JGProgressHUD.h>//;
 
 @interface LoginViewController ()
 
@@ -36,12 +37,42 @@
 */
 
 - (IBAction)loginButton:(id)sender {
+    __block NSString *passwordFromServer;
+    
     PFQuery *query = [PFQuery queryWithClassName:@"Account"];
     [query whereKey:@"username" equalTo:_usernameText.text];
     [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error){
-        NSLog(@"RETURNED USERNAME:%@",[object objectForKey:@"username"]);
-        NSLog(@"RETURNED PASSWORD:%@",[object objectForKey:@"password"]);
+        passwordFromServer = (NSString *)[object objectForKey:@"password"];
+        NSLog(@"RETURNED USERNAME:%@ RETURNED PASSWORD:%@",[object objectForKey:@"username"],passwordFromServer);
+        NSLog(@"Typed password:%@", _passwordText.text);
+        if(object == nil){
+            JGProgressHUD *HUD = [JGProgressHUD progressHUDWithStyle:JGProgressHUDStyleDark];
+            HUD.textLabel.text = @"Error: Username";
+            HUD.indicatorView = [[JGProgressHUDErrorIndicatorView alloc] init]; //JGProgressHUDSuccessIndicatorView is also available
+            [HUD showInView:self.view];
+            [HUD dismissAfterDelay:2.0];
+        }
+        else if(![passwordFromServer isEqualToString:_passwordText.text]){
+            JGProgressHUD *HUD = [JGProgressHUD progressHUDWithStyle:JGProgressHUDStyleDark];
+            HUD.textLabel.text = @"Error: Password";
+            HUD.indicatorView = [[JGProgressHUDErrorIndicatorView alloc] init]; //JGProgressHUDSuccessIndicatorView is also available
+            [HUD showInView:self.view];
+            [HUD dismissAfterDelay:2.0];
+        }
+        else{
+            JGProgressHUD *HUD = [JGProgressHUD progressHUDWithStyle:JGProgressHUDStyleDark];
+            HUD.textLabel.text = @"Success!";
+            HUD.indicatorView = [[JGProgressHUDSuccessIndicatorView alloc] init]; //JGProgressHUDSuccessIndicatorView is also available
+            [HUD showInView:self.view];
+            [HUD dismissAfterDelay:2.0];
+            [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(loginSuccess:) userInfo:nil repeats:NO];
+        }
     }];
     
 }
+
+- (void)loginSuccess: (id)sender{
+    [self performSegueWithIdentifier:@"login" sender:self];
+}
+
 @end
--- ios/ParseStarterProject/ParseStarterProject/SignUpViewController.m
+++ ios/ParseStarterProject/ParseStarterProject/SignUpViewController.m
@@ -8,6 +8,7 @@
 
 #import "SignUpViewController.h"
 #import <Parse/Parse.h>
+#import <JGProgressHUD/JGProgressHUD.h>//;
 
 @interface SignUpViewController ()
 
@@ -39,9 +40,32 @@
 */
 
 - (IBAction)signup:(id)sender {
-    PFObject *accountObject = [PFObject objectWithClassName:@"Account"];
-    accountObject[@"username"] = _usernameText.text;
-    accountObject[@"password"] = _passwordText.text;
-    [accountObject saveInBackground];
+    PFQuery *query = [PFQuery queryWithClassName:@"Account"];
+    [query whereKey:@"username" equalTo:_usernameText.text];
+    [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error){
+        
+        if(object == nil){
+            PFObject *accountObject = [PFObject objectWithClassName:@"Account"];
+            accountObject[@"username"] = _usernameText.text;
+            accountObject[@"password"] = _passwordText.text;
+            [accountObject saveInBackground];
+            JGProgressHUD *HUD = [JGProgressHUD progressHUDWithStyle:JGProgressHUDStyleDark];
+            HUD.textLabel.text = @"SignUp Success!";
+            HUD.indicatorView = [[JGProgressHUDSuccessIndicatorView alloc] init]; //JGProgressHUDSuccessIndicatorView is also available
+            [HUD showInView:self.view];
+            [HUD dismissAfterDelay:2.0];
+        }
+        else{
+            JGProgressHUD *HUD = [JGProgressHUD progressHUDWithStyle:JGProgressHUDStyleDark];
+            HUD.textLabel.text = @"Error: Username already exist!";
+            HUD.indicatorView = [[JGProgressHUDErrorIndicatorView alloc] init]; //JGProgressHUDSuccessIndicatorView is also available
+            [HUD showInView:self.view];
+            [HUD dismissAfterDelay:2.0];
+        }
+    }];
+
+    
+    
+    
 }
 @end
